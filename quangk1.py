feb2eb2b0094d575a10eab887939b0130ecb2970
mguyenvinhquangdhv <79903941+mguyenvinhquangdhv@users.noreply.github.com>
2021-03-09 17:26:36 +0700

    Add files via upload

---
+++ quangk1.py
@@ -0,0 +1,6 @@
+n1=int (input ("input n1 value"))
+n2=int (input ("input n2 value"))
+if n1>n2:
+    print ("n1 is big")
+else:
+    print("n2 is big")
---
+++ quangk5.py
@@ -0,0 +1,4 @@
+n=int(input("Enter A number--->"))
+While n>=0;
+    print(n);
+    n=n-1;
